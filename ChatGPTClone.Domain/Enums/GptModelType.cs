dc89dfa1b192bbde64ab98c6c6b0f37b422e6d65
umutatras <umutatras@gmail.com>
2024-08-17 12:35:02 +0300

    GptModelType enum eklendi

--- ChatGPTClone.Domain/Enums/GptModelType.cs
+++ ChatGPTClone.Domain/Enums/GptModelType.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChatGPTClone.Domain.Enums
+﻿namespace ChatGPTClone.Domain.Enums
 {
     public enum GptModelType
     {
+        GPT4o=1,
+        GPT4oMini=2,
+        GPT4=3,
+        
     }
 }
