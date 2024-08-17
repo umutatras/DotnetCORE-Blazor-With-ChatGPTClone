660c8b6b1258677345bd71500278d9983bd02500
umutatras <umutatras@gmail.com>
2024-08-17 12:40:41 +0300

    ChatMessageType oluşturuldu

---
+++ ChatGPTClone.Domain/Enums/ChatMessageType.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatGPTClone.Domain.Enums
+{
+    public enum ChatMessageType
+    {
+        System=1,
+        User=2,
+        Assistant=3
+    }
+}
---
+++ ChatGPTClone.Domain/ValueObjects/ChatMessage.cs
@@ -0,0 +1,12 @@
+﻿using ChatGPTClone.Domain.Enums;
+
+namespace ChatGPTClone.Domain.ValueObjects
+{
+    public class ChatMessage
+    {
+        public string Id { get; set; }
+        public ChatMessageType Type { get; set; }
+        public string Content { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+    }
+}
