0ed04a224dd4731055fe656361eca88ab4bb4ad0
umutatras <umutatras@gmail.com>
2024-08-17 12:06:00 +0300

    BaseEntity oluşturuldu

---
+++ ChatGPTClone.Domain/Common/EntityBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Domain.Common
+{
+    public class EntityBase
+    {
+        public Guid Id { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+        public string CreatedByUserId { get; set; }
+        public DateTimeOffset? ModifiedOn { get; set; }
+        public string? ModifiedByUserId { get; set; }
+    }
+}
