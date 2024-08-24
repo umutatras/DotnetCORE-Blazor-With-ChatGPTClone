bd8436ce856eb39e3933bb85d6016dca2b3bf952
umutatras <umutatras@gmail.com>
2024-08-24 12:13:19 +0300

    ChatSessionCreateCommand created

---
+++ ChatGPTClone.Application/Features/ChatSessions/Commands/Create/ChatSessionCreateCommand.cs
@@ -0,0 +1,66 @@
+﻿using ChatGPTClone.Domain.Entities;
+using ChatGPTClone.Domain.Enums;
+using ChatGPTClone.Domain.ValueObjects;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Features.ChatSessions.Commands.Create
+{
+    public class ChatSessionCreateCommand : IRequest<Guid>
+    {
+        public string Content { get; set; }
+        public GptModelType Model { get; set; }
+
+        public ChatSession ToChatSession(Guid userId)
+        {
+            return new ChatSession()
+            {
+                Id = Guid.NewGuid(),
+                Model = Model,
+                AppUserId = userId,
+                CreatedOn = DateTimeOffset.UtcNow,
+                CreatedByUserId = userId.ToString(),
+                Title = Content.Length >= 50 ? Content.Substring(0, 50) : Content,
+                Threads =
+                [
+                    new ChatThread()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        CreatedOn = DateTimeOffset.UtcNow,
+                        Messages =
+                        [
+                            new ChatMessage()
+                            {
+                                Id = Guid.NewGuid().ToString(),
+                                Model = Model,
+                                Type = ChatMessageType.System,
+                                Content = "You're a very helpful and happy assistant which loves to help people.",
+                                CreatedOn = DateTimeOffset.UtcNow
+                            }
+                        ]
+                    },
+                    new ChatThread()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        CreatedOn = DateTimeOffset.UtcNow,
+                        Messages =
+                        [
+                            new ChatMessage()
+                            {
+                                Id = Guid.NewGuid().ToString(),
+                                Model = Model,
+                                Type = ChatMessageType.User,
+                                Content = Content,
+                                CreatedOn = DateTimeOffset.UtcNow
+                            }
+                        ]
+                    }
+                ]
+            };
+        }
+    }
+}
---
+++ ChatGPTClone.Application/Features/ChatSessions/Commands/Create/ChatSessionCreateCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using ChatGPTClone.Application.Common.Interfaces;
+using MediatR;
+
+namespace ChatGPTClone.Application.Features.ChatSessions.Commands.Create
+{
+    public class ChatSessionCreateCommandHandler : IRequestHandler<ChatSessionCreateCommand, Guid>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public ChatSessionCreateCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Guid> Handle(ChatSessionCreateCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
