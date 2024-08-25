987768b2dcef3cabf5b3fec36eb7ad13a3ae1b07
umutatras <umutatras@gmail.com>
2024-08-25 10:38:37 +0300

    ICurrentUserService,CurrentUserManager created

---
+++ ChatGPTClone.WebApi/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using ChatGPTClone.Application.Common.Interfaces;
+using ChatGPTClone.WebApi.Services;
+
+namespace ChatGPTClone.WebApi
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddWebApi(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddScoped<ICurrentUserService, CurrentUserManager>();
+            return services;
+        }
+    }
+}
--- ChatGPTClone.WebApi/Program.cs
+++ ChatGPTClone.WebApi/Program.cs
@@ -1,4 +1,5 @@
 using ChatGPTClone.Infrastructure;
+using ChatGPTClone.WebApi;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddWebApi(builder.Configuration);
 
 var app = builder.Build();
 
