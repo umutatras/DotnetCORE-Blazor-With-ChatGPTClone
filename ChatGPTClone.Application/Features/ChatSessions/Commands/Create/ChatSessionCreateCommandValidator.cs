620e443dab96dd932e57c4b2ad50a13551d880c2
umutatras <umutatras@gmail.com>
2024-08-25 10:25:39 +0300

    ChatSessionCreateCommandValidator created

---
+++ ChatGPTClone.Application/Features/ChatSessions/Commands/Create/ChatSessionCreateCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace ChatGPTClone.Application.Features.ChatSessions.Commands.Create
+{
+    public class ChatSessionCreateCommandValidator : AbstractValidator<ChatSessionCreateCommand>
+    {
+        public ChatSessionCreateCommandValidator()
+        {
+            RuleFor(x=>x.Model).IsInEnum().WithMessage("Model is invalid").NotNull().WithMessage("Model is required").NotEmpty().WithMessage("Model is required");
+
+            RuleFor(x => x.Content)
+    .NotNull()
+    .WithMessage("Content is required.")
+    .NotEmpty()
+    .WithMessage("Content is required.")
+    .MaximumLength(4000)
+    .WithMessage("Content must not exceed 4000 characters.");
+        }
+    }
+}
