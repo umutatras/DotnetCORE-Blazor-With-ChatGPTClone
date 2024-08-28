bd7db8495493b0c5a1158689ffa0ae22d4c9b856
umutatras <umutatras@gmail.com>
2024-08-28 21:21:49 +0300

    serilog projeye eklendi

--- ChatGPTClone.WebApi/Program.cs
+++ ChatGPTClone.WebApi/Program.cs
@@ -2,34 +2,55 @@ using ChatGPTClone.Infrastructure;
 using ChatGPTClone.Application;
 using ChatGPTClone.WebApi;
 using ChatGPTClone.WebApi.Filters;
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
 
-// Add services to the container.
-
-builder.Services.AddControllers(opt =>
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+try
 {
-    opt.Filters.Add<GlobalExceptionFilter>();
-});
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddApplicaton();
-builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddWebApi(builder.Configuration);
+    Log.Information("Starting web application");
+    var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+    builder.Services.AddSerilog();
+    // Add services to the container.
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddControllers(opt =>
+    {
+        opt.Filters.Add<GlobalExceptionFilter>();
+    });
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddApplicaton();
+    builder.Services.AddInfrastructure(builder.Configuration);
+    builder.Services.AddWebApi(builder.Configuration);
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
 }
+catch (Exception ex)
+{
 
-app.UseHttpsRedirection();
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+}
