96b2290d031f8a9be69d48a5152b181226b45c40
umutatras <umutatras@gmail.com>
2024-08-31 10:51:28 +0300

    Localization ayarlamaları yapılıyor v1

---
+++ ChatGPTClone.Application/Common/Localization/CommonLocalization.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Common.Localization
+{
+    public class CommonLocalization
+    {
+        public CommonLocalization()
+        {
+            
+        }
+    }
+}
---
+++ ChatGPTClone.Application/Common/Localization/CommonLocalizationKeys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Common.Localization
+{
+    public static class CommonLocalizationKeys
+    {
+        public static string GeneralValidationExceptionMessage => nameof(GeneralValidationExceptionMessage);
+        public static string GeneralNotFoundExceptionMessage => nameof(GeneralNotFoundExceptionMessage);
+        public static string GeneralInternalServerExceptionMessage => nameof(GeneralInternalServerExceptionMessage);
+    }
+}
