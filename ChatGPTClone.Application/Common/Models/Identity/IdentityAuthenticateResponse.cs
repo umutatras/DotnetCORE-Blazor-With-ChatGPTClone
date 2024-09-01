33e847322ef3f526af15579ef33b7b040bd83317
umutatras <umutatras@gmail.com>
2024-09-01 11:35:38 +0300

    Jwt token modelleri ve ayarlamaları yapılıyor v1

---
+++ ChatGPTClone.Application/Common/Models/Identity/IdentityAuthenticateResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatGPTClone.Application.Common.Models.Identity
+{
+    public class IdentityAuthenticateResponse
+    {
+        public string Token { get; set; }
+        //public IdentityAuthenticateResponse(string token)
+        //{
+        //    Token = token;  
+        //}
+    }
+}
