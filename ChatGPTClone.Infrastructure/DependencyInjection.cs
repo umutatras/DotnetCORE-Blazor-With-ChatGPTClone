7518510faf766cb6fb967ffc86596e2fa4541f5f
umutatras <umutatras@gmail.com>
2024-09-01 12:23:29 +0300

    jwt config

--- ChatGPTClone.Infrastructure/DependencyInjection.cs
+++ ChatGPTClone.Infrastructure/DependencyInjection.cs
@@ -16,7 +16,32 @@ namespace ChatGPTClone.Infrastructure
             services.AddDbContext<ApplicationDbContext>(opt => opt.UseNpgsql(connectionString));
             services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
             services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+            //ConfigureJwtSettings(services,configuration);
             return services;
         }
+        // JWT ayarlarını yapılandıran özel metod
+        //private static void ConfigureJwtSettings(IServiceCollection services, IConfiguration configuration)
+        //{
+        //    // Yapılandırmadan JWT ayarları bölümünü alır
+        //    var jwtSettingsSection = configuration.GetSection("JwtSettings");
+
+        //    // Eğer JWT ayarları yapılandırmada mevcutsa, bu ayarları kullanır
+        //    if (jwtSettingsSection.Exists())
+        //    {
+        //        services.Configure<JwtSettings>(jwtSettingsSection);
+        //    }
+        //    // Aksi takdirde, varsayılan değerlerle JWT ayarlarını yapılandırır
+        //    else
+        //    {
+        //        services.Configure<JwtSettings>(options =>
+        //        {
+        //            options.SecretKey = "default-secret-key-for-development-only";
+        //            options.AccessTokenExpiration = TimeSpan.FromMinutes(30);
+        //            options.RefreshTokenExpiration = TimeSpan.FromDays(7);
+        //            options.Issuer = "ChatGPTClone";
+        //            options.Audience = "ChatGPTClone";
+        //        });
+        //    }
+        //}
     }
 }
