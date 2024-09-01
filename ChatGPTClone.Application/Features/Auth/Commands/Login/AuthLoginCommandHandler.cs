816ec27c4da7a81d47dfecaa00c21b19575b568a
umutatras <umutatras@gmail.com>
2024-09-01 11:53:07 +0300

    login validator

---
+++ ChatGPTClone.Application/Features/Auth/Commands/Login/AuthLoginCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using ChatGPTClone.Application.Common.Interfaces;
+using ChatGPTClone.Application.Common.Models.General;
+using ChatGPTClone.Application.Common.Models.Identity;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Features.Auth.Commands.Login
+{
+    public class AuthLoginCommandHandler : IRequestHandler<AuthLoginCommand, ResponseDto<AuthLoginDto>>
+    {
+        private readonly IIdentityService _identityService;
+
+        public AuthLoginCommandHandler(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        public async Task<ResponseDto<AuthLoginDto>> Handle(AuthLoginCommand request, CancellationToken cancellationToken)
+        {
+            var response = await _identityService.LoginAsync(request.ToIdentityLoginRequest(), cancellationToken);
+            return new ResponseDto<AuthLoginDto>(AuthLoginDto.FromIdentityLoginResponse(response));
+        }
+    }
+}
