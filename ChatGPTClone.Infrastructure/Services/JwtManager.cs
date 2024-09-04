bb8e61125a5a61a25c4245d194c05e2d9f08cc59
umutatras <umutatras@gmail.com>
2024-09-04 20:36:50 +0300

    GenerateToken class added

--- ChatGPTClone.Infrastructure/Services/JwtManager.cs
+++ ChatGPTClone.Infrastructure/Services/JwtManager.cs
@@ -2,6 +2,10 @@
 using ChatGPTClone.Application.Common.Models.Jwt;
 using ChatGPTClone.Domain.Settings;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace ChatGPTClone.Infrastructure.Services
 {
@@ -16,7 +20,30 @@ namespace ChatGPTClone.Infrastructure.Services
 
         public JwtGenerateTokenResponse GenerateToken(JwtGenerateTokenRequest request)
         {
-            throw new NotImplementedException();
+            var expirationInMinutes = _jwtSettings.AccessTokenExpiration;
+            var expirationDate = DateTime.Now.AddMinutes(expirationInMinutes.Minutes);
+            var claims = new List<Claim>
+            {
+                 new Claim("uid", request.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, request.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iss, _jwtSettings.Issuer),
+                new Claim(JwtRegisteredClaimNames.Aud, _jwtSettings.Audience),
+                new Claim(JwtRegisteredClaimNames.Exp, expirationDate.ToFileTime().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.Now.ToFileTime().ToString()),
+            }
+            .Union(request.Roles.Select(role=>new Claim(ClaimTypes.Role,role)));
+
+            var symmetricSecurtiyKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.SecretKey));
+            var signingCredentials = new SigningCredentials(symmetricSecurtiyKey, SecurityAlgorithms.HmacSha256);
+            var jwtSecurityToken = new JwtSecurityToken(issuer:_jwtSettings.Issuer,
+                audience:_jwtSettings.Audience,
+                claims:claims,
+                expires:expirationDate,
+                signingCredentials:signingCredentials);
+
+            var token =new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+            return new JwtGenerateTokenResponse(token, expirationDate);
         }
     }
 }
