d61ef0f09baefb5ada4939af1d1b9d0b45161c58
umutatras <umutatras@gmail.com>
2024-09-04 20:20:45 +0300

    added nuget package for jwt and updated di inf.

---
+++ ChatGPTClone.Infrastructure/Services/JwtManager.cs
@@ -0,0 +1,22 @@
+﻿using ChatGPTClone.Application.Common.Interfaces;
+using ChatGPTClone.Application.Common.Models.Jwt;
+using ChatGPTClone.Domain.Settings;
+using Microsoft.Extensions.Options;
+
+namespace ChatGPTClone.Infrastructure.Services
+{
+    public class JwtManager : IJwtService
+    {
+        private readonly JwtSettings _jwtSettings;
+
+        public JwtManager(IOptions<JwtSettings> jwtSettings)
+        {
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        public JwtGenerateTokenResponse GenerateToken(JwtGenerateTokenRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
