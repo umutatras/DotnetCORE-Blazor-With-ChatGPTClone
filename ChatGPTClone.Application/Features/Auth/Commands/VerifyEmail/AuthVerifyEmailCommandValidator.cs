14de25559315b8b12156462db0dc1a8c4b8c2c00
umutatras <umutatras@gmail.com>
2024-09-08 10:45:55 +0300

    AuthVerifyEmailCommandValidator yazıldı

---
+++ ChatGPTClone.Application/Features/Auth/Commands/VerifyEmail/AuthVerifyEmailCommandValidator.cs
@@ -0,0 +1,32 @@
+﻿using ChatGPTClone.Application.Common.Interfaces;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTClone.Application.Features.Auth.Commands.VerifyEmail
+{
+    public class AuthVerifyEmailCommandValidator:AbstractValidator<AuthVerifyEmailCommand>
+    {
+        private readonly IIdentityService _identityService;
+
+        public AuthVerifyEmailCommandValidator(IIdentityService identityService)
+        {
+            _identityService = identityService;
+
+            RuleFor(x => x.Email)
+            .NotEmpty().EmailAddress()
+            .MustAsync(EmailExists).WithMessage("Email not found.");
+
+            RuleFor(x => x.Token).NotEmpty();
+
+        }
+
+        private Task<bool> EmailExists(string email, CancellationToken cancellationToken)
+        {
+            return _identityService.CheckEmailExistsAsync(email, cancellationToken);
+        }
+    }
+}
