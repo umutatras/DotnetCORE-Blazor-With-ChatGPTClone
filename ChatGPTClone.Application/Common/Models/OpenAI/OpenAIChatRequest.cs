f3ca907e722b6bfd0a2b6fe99e85fb9e5accabee
umutatras <umutatras@gmail.com>
2024-09-18 20:09:36 +0300

    open ai oluşturuldu

---
+++ ChatGPTClone.Application/Common/Models/OpenAI/OpenAIChatRequest.cs
@@ -0,0 +1,11 @@
+﻿using ChatGPTClone.Domain.Enums;
+
+namespace ChatGPTClone.Application.Common.Models.OpenAI
+{
+    public class OpenAIChatRequest
+    {
+        public GptModelType Model { get; set; }
+        public string Message { get; set; }
+        public List<ChatMessageType> Messages { get; set; }
+    }
+}
