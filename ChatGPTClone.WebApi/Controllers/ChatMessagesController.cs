d58f08467d7d8e59039a8a56540bc90c7bbf6bcd
umutatras <umutatras@gmail.com>
2024-09-21 11:46:33 +0300

    ChatMessagesController oluşturuldu

---
+++ ChatGPTClone.WebApi/Controllers/ChatMessagesController.cs
@@ -0,0 +1,18 @@
+﻿using ChatGPTClone.Application.Features.ChatMessages.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatGPTClone.WebApi.Controllers
+{
+    [Authorize]
+    public class ChatMessagesController : ApiControllerBase
+    {
+        public ChatMessagesController(ISender mediatr) : base(mediatr)
+        {
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ChatMessageCreateCommand command, CancellationToken token) => Ok(await Mediatr.Send(command, token));
+    }
+}
