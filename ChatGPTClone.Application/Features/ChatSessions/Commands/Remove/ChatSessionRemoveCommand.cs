af911eebc8e5720214ebbefe3727bea410b5094b
umutatras <umutatras@gmail.com>
2024-09-22 12:03:43 +0300

    ChatSessionRemoveCommand yapıldı

---
+++ ChatGPTClone.Application/Features/ChatSessions/Commands/Remove/ChatSessionRemoveCommand.cs
@@ -0,0 +1,16 @@
+﻿using ChatGPTClone.Application.Common.Models.General;
+using MediatR;
+
+namespace ChatGPTClone.Application.Features.ChatSessions.Commands.Remove
+{
+    public class ChatSessionRemoveCommand : IRequest<ResponseDto<Guid>>
+    {
+        public Guid Id { get; set; }
+
+        public ChatSessionRemoveCommand(Guid id)
+        {
+            Id = id;
+        }
+
+    }
+}
--- ChatGPTClone.Application/Features/ChatSessions/Queries/GetById/ChatSessionGetByIdQueryValidator.cs
+++ ChatGPTClone.Application/Features/ChatSessions/Queries/GetById/ChatSessionGetByIdQueryValidator.cs
@@ -4,10 +4,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ChatGPTClone.Application.Features.ChatSessions.Queries.GetById
 {
-    public class ChatSessionGetByIdQueryValidator:AbstractValidator<ChatSessinGetByIdQuery>
+    public class ChatSessionGetByIdQueryValidator : AbstractValidator<ChatSessinGetByIdQuery>
     {
         private readonly IApplicationDbContext _context;
-        public ChatSessionGetByIdQueryValidator(IApplicationDbContext context)
+        private readonly ICurrentUserService _currentUserService;
+        public ChatSessionGetByIdQueryValidator(IApplicationDbContext context, ICurrentUserService currentUserService)
         {
             _context = context;
 
@@ -16,10 +17,11 @@ namespace ChatGPTClone.Application.Features.ChatSessions.Queries.GetById
                 .NotNull()
                 .MustAsync(BeValidIdAsync)
                 .WithMessage("The selected Chat was not found.");
+            _currentUserService = currentUserService;
         }
-        private Task<bool> BeValidIdAsync(Guid id,CancellationToken cancellationToken)
+        private Task<bool> BeValidIdAsync(Guid id, CancellationToken cancellationToken)
         {
-            return _context.ChatSessions.AnyAsync(x=>x.Id == id,cancellationToken);
+            return _context.ChatSessions.AnyAsync(x => x.Id == id && x.AppUserId == _currentUserService.UserId, cancellationToken);
         }
     }
 }
