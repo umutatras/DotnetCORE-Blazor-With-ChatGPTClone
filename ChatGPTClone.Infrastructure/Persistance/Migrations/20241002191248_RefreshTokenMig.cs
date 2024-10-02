1e5c3c55ebf5ab71d331bc002dce84d710339a73
umutatras <umutatras@gmail.com>
2024-10-02 22:13:39 +0300

    refresh token migrations ekledi

---
+++ ChatGPTClone.Infrastructure/Persistance/Migrations/20241002191248_RefreshTokenMig.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ChatGPTClone.Infrastructure.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefreshTokenMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RefreshTokens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Token = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Expires = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedByIp = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false),
+                    SecurityStamp = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Revoked = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    RevokedByIp = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: true),
+                    AppUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ModifiedOn = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    ModifiedByUserId = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: true),
+                    CreatedOn = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreatedByUserId = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RefreshTokens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RefreshTokens_Users_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("2798212b-3e5d-4556-8629-a64eb70da4a8"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "e3980858-8c54-4454-8ec5-140012cc92de", "AQAAAAIAAYagAAAAEDGK2UkvHqc4KafHCLEOXPenUniLENxBr68ak8THkwN17gXEh4EXCoVs9xm1MUylCA==", "7e8bd166-33a1-4307-a25a-e2e0345e24e6" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_AppUserId_Token",
+                table: "RefreshTokens",
+                columns: new[] { "AppUserId", "Token" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_Token",
+                table: "RefreshTokens",
+                column: "Token",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RefreshTokens");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("2798212b-3e5d-4556-8629-a64eb70da4a8"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "e4ff6959-7ef4-4d26-89f0-81b9fe15d9f4", "AQAAAAIAAYagAAAAEP4Bj7bF6wkzUH79Yy4M0u9ffnW1PQbRmjGVF+DBORiogH5H7e9Ct0dHhJs+RVlquQ==", "6a17d88e-ac30-43c4-9442-4de3cad7aeaa" });
+        }
+    }
+}
